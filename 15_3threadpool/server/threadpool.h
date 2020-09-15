e8e3fc94fbb26dc341adac63ec6893cabde9e6c7
vodake <doubix97@icloud.com>
2020-09-15 14:46:05 +0800

    实现threadpool

---
+++ 15_3threadpool/server/threadpool.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#include<list>
+#include<cstdio>
+#include<exception>
+#include<pthread.h>
+
+
+#include "locker.h"
+
+template<typename T>
+class ThreadPool
+{
+public:
+    ThreadPool(int thread_number = 8, int max_request = 10000);
+    ~ThreadPool();
+    bool append(T *request);
+
+
+private:
+    static void* worker(void* arg);
+    void run();
+
+    int m_thread_number;
+    int m_max_requests;
+    pthread_t *m_threads;
+    std::list<T *> m_workqueue;
+    locker m_quenelocker;
+    sem m_queuestat;
+    bool m_stop;
+};
+
+template<typename T>
+ThreadPool<T>::ThreadPool(int thread_number, int max_requests):
+    m_thread_number(thread_number),m_max_requests(max_requests),m_stop(false),m_thread(nullptr)
+{
+    if((thread_number<=0)||(max_requests<=0))
+    {
+        throw std::exception();
+    }
+
+    m_threads = new pthread_t[m_thread_number];
+    if(!m_threads)
+    {
+        throw std::exception();
+    }
+
+    for (int i = 0; i < thread_number;++i)
+    {
+        cout << "create the " << i << "th thread" << endl;
+        if(pthread_create(m_threads+i,nullptr,worker,this)!=0)
+        {
+            delete[] m_threads;
+            throw std::exception();
+        }
+        if(pthread_detach(m_threads[i]))
+        {
+            delete[] m_threads;
+            throw std::exception();
+        }
+    }
+}
+
+template<typename T>
+ThreadPool<T>::~ThreadPool()
+{
+    delete[] m_threads;
+    m_stop = true;
+}
+
+
+template<typename T>
+bool ThreadPool<T>::append(T* request)
+{
+    m_quenelocker.lock();
+    if(m_workqueue.size()>m_max_requests)
+    {
+        m_quenelocker.unlock();
+        return false;
+    }
+    m_workqueue.push_back(request);
+    m_quenelocker.unlock();
+    m_queuestat.post();
+    return true;
+}
+
+template<typename T>
+void ThreadPool<T>::run()
+{
+    while(!m_stop)
+    {
+        m_queuestat.wait();
+        m_quenelocker.lock();
+        if(m_workqueue.empty())
+        {
+            m_quenelocker.unlock();
+            continue;
+        }
+        T *request = m_workqueue.front();
+        m_workqueue.pop_front();
+        m_quenelocker.unlock();
+        if(!request)
+        {
+            continue;
+        }
+        request->process();
+    }
+}
